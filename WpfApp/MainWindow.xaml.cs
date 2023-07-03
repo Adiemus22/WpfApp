564fcff2c1ef47831491cb50c74dca71dd219bb5
Adiemus22 <reneriedel@posteo.de>
2023-07-04 01:43:15 +0200

    Add project files.

---
+++ WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+
+namespace WpfApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DarkModeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Resources.MergedDictionaries[0].Source = new Uri("Styles/Dark.xaml", UriKind.Relative);
+        }
+
+        private void BrightModeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Resources.MergedDictionaries[0].Source = new Uri("Styles/Bright.xaml", UriKind.Relative);
+        }
+    }
+}
