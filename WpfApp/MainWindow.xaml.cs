1fc43261040c04ff9f761a052e9c3142c780d4e1
Adiemus22 <reneriedel@posteo.de>
2023-07-04 02:04:31 +0200

    qswd

--- WpfApp/MainWindow.xaml.cs
+++ WpfApp/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ namespace WpfApp
         private void DarkModeButton_Click(object sender, RoutedEventArgs e)
         {
             Application.Current.Resources.MergedDictionaries[0].Source = new Uri("Styles/Dark.xaml", UriKind.Relative);
+            Button1.Content = "OK";
         }
 
         private void BrightModeButton_Click(object sender, RoutedEventArgs e)
